1ae5497601e12c0ad7f63ca81ec281fc04a20d6b
Alessio Biancalana <alessio@dottorblaster.it>
2024-02-02 09:47:52 +0100

    Use RSS feed URL as parameter

--- src/feed.rs
+++ src/feed.rs
@@ -1,10 +1,8 @@
 use crate::Sort;
 use rss::{Channel, Item};
 
-const FEED_URL: &str = "https://www.archlinux.org/feeds/news/";
-
-pub fn get_items(sort: &Sort) -> Result<Vec<Item>, Box<dyn std::error::Error>> {
-    let content = reqwest::blocking::get(FEED_URL)?.bytes()?;
+pub fn get_items(feed_url: &str, sort: &Sort) -> Result<Vec<Item>, Box<dyn std::error::Error>> {
+    let content = reqwest::blocking::get(feed_url)?.bytes()?;
     let channel = Channel::read_from(&content[..])?;
     match sort {
         Sort::Desc => Ok(channel.into_items().into_iter().rev().collect()),
